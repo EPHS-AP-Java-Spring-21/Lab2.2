4b8c134171bd8ec4e75806a0afe06507d8c31bdc
MrMayRRHS <32204762+MrMayRRHS@users.noreply.github.com>
2020-09-29 19:11:43 -0500

    Add files via upload

---
+++ Greeter.java
@@ -0,0 +1,15 @@
+/* Write the Greeter class in this file.
+ * This class should have 2 methods in it.
+ * 
+ * sayHi method
+ * Returns nothing
+ * Takes no paramters
+ * Outputs the message "Hello" using System.out.println
+ * Note: this method is one of the few methods that should use System.out.println
+ * 
+ * greet method
+ * Returns nothing
+ * Takes a String parameter called name
+ * Outputs the message "Hello [NAME]" where [NAME] is the value of the name parameter
+ * Note: this method is one of the few methods that should use System.out.println
+ */
---
+++ GreeterTester.java
@@ -0,0 +1,16 @@
+/* DO NOT CHANGE ANYTHING IN THIS FILE
+ * If you are getting an error in this file, it is becuase
+ * of a mistake in your Greeter.java file!
+ */
+public class GreeterTester
+{
+  public static void main(String[] args)
+  {
+    // Instantiate a Greeter Object
+    //Greeter g = new Greeter();
+    
+    // Invoke the Greeter object's methods
+    //g.sayHi();
+    //g.greet("Cam Newton");
+  }
+}
